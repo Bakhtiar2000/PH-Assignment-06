a6de974f4183bbcf00bf72881fefc1ddd6dbe7d1
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-03-02 21:52:25 +0600

    main section added and fetched url

--- js/universe.js
+++ js/universe.js
@@ -1,1 +1,20 @@
+//Fetch operation using async await keywords
 
+const loadAiInformation= async()=> {
+    try{
+        const url= `https://openapi.programming-hero.com/api/ai/tools`;
+
+        const res= await fetch(url);
+        const data= await res.json();
+        displayAiInformation(data.data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+const displayAiInformation= (data)=>{
+    console.log(data);
+}
+
+loadAiInformation();
