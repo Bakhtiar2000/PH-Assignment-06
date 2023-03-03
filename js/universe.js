53a1f9e139128037b13c27af0a0d1b1120cc39f7
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-03-04 00:19:28 +0600

    modal working perfectly

--- js/universe.js
+++ js/universe.js
@@ -24,7 +24,7 @@ const displayAiInformation= (info)=>{
         seeMoreButton.classList.add('d-none');
     }
     info.forEach(data => {
-        console.log(data.id);
+        // console.log(data.id);
         
         const boxDiv= document.createElement('div');
         boxDiv.classList.add('col');
@@ -93,46 +93,53 @@ const loadAiInformationById= async(id)=> {
 }
 
 const displayAiInformationById= (info)=>{
-    console.log(info.features[1]);
+    console.log(info);
 
     const aiDetails= document.getElementById('ai-details');
         aiDetails.innerHTML=`
-        
+        <div class="border border-danger-subtle rounded-2 p-3" style="width: 100%; background-color: #f5dfcf57;">
+                    <p class="fs-4 fw-bold text-center">${info.description? info.description: 'Description Unavailable'}</p>
+                    <div class="d-flex justify-content-center align-items-center gap-3">
+                        <div class="rounded-2 bg-white p-1 p-md-3 text-center text-success fw-semibold">
+                            <p>${info.pricing[0].price? info.pricing[0].price: 'Free of Cost/'}</p>
+                            <p>${info.pricing[0].plan? info.pricing[0].plan: 'Basic'}</p>
+                        </div>
+                        <div class="rounded-2 bg-white  p-1 p-md-3 text-center text-primary fw-semibold">
+                            <p>${info.pricing[1].price? info.pricing[1].price: 'Free of Cost/'}</p>
+                            <p>${info.pricing[1].plan? info.pricing[1].plan: 'Pro'}</p>
+                        </div>
+                        <div class="rounded-2 bg-white  p-1 p-md-3 text-center text-warning fw-semibold">
+                            <p>${info.pricing[2].price? info.pricing[2].price: 'Free of Cost/'}</p>
+                            <p>${info.pricing[2].plan? info.pricing[2].plan: 'Enterprise'}</p>
+                        </div>
+                    </div>
+            
+                    <div class="mt-3 d-flex justify-content-around gap-3 text-start">
+                        <div class=" p-2">
+                            <h3 class="fs-3 fw-semibold">Features</h3>
+                            <ul>
+                                <li>${info.features[1].feature_name? info.features[1].feature_name: 'No Data Found'}</li>
+                                <li>${info.features[2].feature_name? info.features[2].feature_name: 'No Data Found'}</li>
+                                <li>${info.features[3].feature_name? info.features[3].feature_name: 'No Data Found'}</li>
+                            </ul>
+                        </div>
+                        <div class=" p-2">
+                            <h3 class="fs-3 fw-semibold">Integrations </h3>
+                            <ul>
+                                <li>${info.integrations[0]? info.integrations[0]: 'No Data Found'}</li>
+                                <li>${info.integrations[1]? info.integrations[1]: 'No Data Found'}</li>
+                                <li>${info.integrations[2]? info.integrations[2]: 'No Data Found'}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                <div class="border border-secondary-subtle rounded-2 p-3" style="width: 100%">
+                        <div class="d-flex justify-content-center position-relative">
+                            <img src="${info.image_link[0]}" class="img-fluid rounded-2">
+                            <div style="top: 1%; right:1%; transform: translate(-1%, -1%);" class="bg-danger text-white py-1 px-3 rounded-2 mt-2 position-absolute">${info.accuracy.score*100}% Accuracy</div>
+                        </div>
+                        <h3 class="mt-3 fs-3 fw-semibold">${info.input_output_examples[0].input? info.input_output_examples[0].input: 'No examples to show'}</h3>
+                        <p>${info.input_output_examples[1].output? info.input_output_examples[0].output: 'No! Not Yet! Take a break!!!'}</p>
+                    </div>
         `
 }
-
-/* <div class="p-3">
-        <p class="fs-2 text-center">${info.description? info.description: 'Description Unavailable'}</p>
-        <div class="fs-4 d-flex justify-content-center">
-            <div class="p-3 text-center text-success fw-semibold">
-                <p>${info.pricing[0].price? info.pricing[0].price: 'Free of Cost/ Basic'}</p>
-            </div>
-            <div class="p-3 text-center text-primary fw-semibold">
-                <p>${info.pricing[1].price? info.pricing[1].price: 'Free of Cost/ Pro'}</p>
-            </div>
-            <div class="p-3 text-center text-warning fw-semibold">
-                <p>${info.pricing[2].price? info.pricing[2].price: 'Free of Cost/ Enterprize'}</p>
-            </div>
-        </div>
-
-        <div>
-            <div>
-                <h3 class="fs-2 fw-semibold">Features</h3>
-                <ul>
-                    <li>${info.features[1].feature_name? info.features[1].feature_name: 'No Data Found'}</li>
-                    <li>${info.features[2].feature_name? info.features[2].feature_name: 'No Data Found'}</li>
-                </ul>
-            </div>
-            <div>
-                <h3 class="fs-2 fw-semibold">Integrations </h3>
-                <ul>
-                    <li>${info.integrations[0].price? info.integrations[0].price: 'No Data Found'}</li>
-                    <li>${info.integrations[1].price? info.integrations[1].price: 'No Data Found'}</li>
-                    <li>${info.integrations[2].price? info.integrations[2].price: 'No Data Found'}</li>
-                </ul>
-            </div>
-        </div>
-        </div>
-        <div class="p-3">
-            <img src="${info.image_link[0]}" alt="">
-        </div> */
